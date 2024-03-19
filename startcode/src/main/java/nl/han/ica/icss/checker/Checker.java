5f5d0454064d29e31991eb2a823d7f0d5d58083a
Niels van Kuppevelt <NC.vanKuppevelt@student.han.nl>
2024-03-19 15:13:35 +0100

    Checkerfuncties deels uitgewerkt

--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -2,10 +2,10 @@ package nl.han.ica.icss.checker;
 
 import nl.han.ica.datastructures.HANLinkedList;
 import nl.han.ica.datastructures.IHANLinkedList;
-import nl.han.ica.icss.ast.AST;
-import nl.han.ica.icss.ast.ASTNode;
-import nl.han.ica.icss.ast.Stylerule;
-import nl.han.ica.icss.ast.Stylesheet;
+import nl.han.ica.icss.ast.*;
+import nl.han.ica.icss.ast.selectors.ClassSelector;
+import nl.han.ica.icss.ast.selectors.IdSelector;
+import nl.han.ica.icss.ast.selectors.TagSelector;
 import nl.han.ica.icss.ast.types.ExpressionType;
 
 import java.util.HashMap;
@@ -28,6 +28,31 @@ public class Checker {
     }
 
     private void check(Stylerule rule) {
+        for (ASTNode node : rule.getChildren()) {
+            if (node instanceof Selector) check ((Selector) node);
+            else if (node instanceof Declaration) check ((Declaration) node);
+        }
+    }
+
+    private void check(Selector selector) {
+        if (selector instanceof TagSelector) check((TagSelector) selector);
+        else if (selector instanceof IdSelector) check((IdSelector) selector);
+        else if (selector instanceof ClassSelector) check((ClassSelector) selector);
+    }
+
+    private void check(TagSelector tagSelector) {
+
+    }
+
+    private void check(IdSelector idSelector) {
+
+    }
+
+    private void check(ClassSelector classSelector) {
+
+    }
+
+    private void check(Declaration declaration) {
 
     }
 
