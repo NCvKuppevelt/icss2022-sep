20e1460e8bb7565e395bee354f2bf8a5be7f7c70
Niels van Kuppevelt <NC.vanKuppevelt@student.han.nl>
2024-03-19 12:07:36 +0100

    Checker level 0 begin

--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -3,6 +3,9 @@ package nl.han.ica.icss.checker;
 import nl.han.ica.datastructures.HANLinkedList;
 import nl.han.ica.datastructures.IHANLinkedList;
 import nl.han.ica.icss.ast.AST;
+import nl.han.ica.icss.ast.ASTNode;
+import nl.han.ica.icss.ast.Stylerule;
+import nl.han.ica.icss.ast.Stylesheet;
 import nl.han.ica.icss.ast.types.ExpressionType;
 
 import java.util.HashMap;
@@ -13,9 +16,19 @@ public class Checker {
     private IHANLinkedList<HashMap<String, ExpressionType>> variableTypes;
 
     public void check(AST ast) {
-         variableTypes = new HANLinkedList<>();
+        variableTypes = new HANLinkedList<>();
 
+        check(ast.root);
     }
 
+    private void check(Stylesheet sheet) {
+        for (ASTNode node : sheet.getChildren()) {
+            check((Stylerule) node);
+        }
+    }
+
+    private void check(Stylerule rule) {
+
+    }
 
 }
